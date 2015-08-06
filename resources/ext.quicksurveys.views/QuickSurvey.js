30d58bf92d7b9fef8f2302349af3000f3f1338c2
jdlrobson <jdlrobson@gmail.com>
2015-08-06 15:54:42 -0700

    Load a quick survey on desktop and mobile
    Writes it in a MobileFrontend like way, making use of templates
    to generate $content and using similar named variables that
    MobileFrontend developers will be used to.
    
    Note:
    * This currently pulls in both Hogan and Mustache on mobile.
    * Results are not stored.
    * A survey can only be shown by including quicksurvey in your query string
    These will be fixed in follow ups.
    
    Dependency: Idb37352acecd745beb53aa8d77ea050851448e0d
    Change-Id: Id5c32bb7a662dda8d153490f7c47e972cabc1efd

---
+++ resources/ext.quicksurveys.views/QuickSurvey.js
@@ -0,0 +1,156 @@
+( function ( $ ) {
+	/**
+	 * Extends a class with new methods and member properties.
+	 *
+	 * @param {OO.Class} ParentClass to extend.
+	 * @param {Object} prototype Prototype that should be incorporated into the new Class.
+	 * @ignore
+	 * FIXME: Talk about upstreaming this to oojs ui as a utility function.
+	 * @return {Class}
+	 */
+	function extend( ChildClass, ParentClass, prototype ) {
+		var key;
+
+		OO.inheritClass( ChildClass, ParentClass );
+		for ( key in prototype ) {
+			ChildClass.prototype[key] = prototype[key];
+		}
+		return ChildClass;
+	}
+
+	/**
+	 * @class QuickSurvey
+	 * @inherit OO.ui.StackLayout
+	 */
+	function QuickSurvey( config ) {
+		this.initialize( config );
+	}
+	extend( QuickSurvey, OO.ui.StackLayout, {
+		/**
+		 * Specifies partials (sub-templates) for use by the widget
+		 * @property {Object}
+		 */
+		templatePartials: {
+			initialPanel: mw.template.get( 'ext.quicksurveys.views', 'initialPanel.mustache' ),
+			finalPanel: mw.template.get( 'ext.quicksurveys.views', 'finalPanel.mustache' )
+		},
+		/**
+		 * A set of default options that are merged with config passed into the initialize function.
+		 * This is likely to change so currently no options are documented.
+		 * @cfg {Object} defaults Default options hash.
+		 */
+		defaults: {
+			buttons: [
+				{
+					label: mw.msg( 'ext-quicksurveys-survey-positive' ),
+					data: {
+						answer: 1
+					}
+				},
+				{
+					label: mw.msg( 'ext-quicksurveys-survey-neutral' ),
+					data: {
+						answer: 0
+					}
+				},
+				{
+					label: mw.msg( 'ext-quicksurveys-survey-negative' ),
+					data: {
+						answer: -1
+					}
+				}
+			],
+			templateData: {
+				finalHeading: mw.msg( 'ext-quicksurveys-survey-confirm-msg' ),
+				footer: mw.message( 'ext-quicksurveys-survey-privacy-policy-default-text' ).parse()
+			},
+			PanelLayout: {
+				expanded: false,
+				framed: false,
+				padded: true,
+				classes: [ 'message content' ]
+			},
+			scrollable: false,
+			expanded: false,
+			classes: [ 'panel panel-inline visible' ]
+		},
+		/**
+		 * Initialise a widget.
+		 * @param {Object} config
+		 */
+		initialize: function ( config ) {
+			var buttonSelect, $btnContainer,
+				btns = [];
+
+			this.config = config || {};
+			$.extend( true, this.config, this.defaults );
+
+			// setup initial panel
+			this.initialPanel = this.widget( 'PanelLayout', 'initialPanel' );
+
+			// setup final panel
+			this.finalPanel = this.widget( 'PanelLayout', 'finalPanel' );
+
+			// Set the buttons
+			$btnContainer = this.initialPanel
+				.$element.find( '.survey-button-container' );
+
+			$.each( this.config.buttons, function () {
+				var btn = new OO.ui.ButtonOptionWidget( this );
+				btns.push( btn );
+			} );
+
+			buttonSelect = new OO.ui.ButtonSelectWidget( {
+				items: btns
+			} );
+			buttonSelect.connect( this, {
+				choose: 'onChoose'
+			} );
+			buttonSelect.$element.appendTo( $btnContainer );
+
+			// setup stack
+			QuickSurvey.super.call( this, $.extend( {}, config, {
+				items: [ this.initialPanel, this.finalPanel ]
+			} ) );
+		},
+		/**
+		 * Make a brand spanking new oojs ui widget from a template partial
+		 * @param {String} widgetName a valid OOJS UI widget
+		 * @param {String} [templatePartialName] name of a registered template partial
+		 * @param {Object} [options] further options to be passed to the widget
+		 */
+		widget: function ( widgetName, templatePartialName, options ) {
+			var template,
+				config = $.extend( {}, this.config[widgetName], options );
+
+			if ( templatePartialName ) {
+				template = this.templatePartials[templatePartialName];
+				if ( template ) {
+					config.$content = template.render( this.config.templateData );
+				}
+			}
+			return new OO.ui[widgetName]( config );
+		},
+		/**
+		 * @param {Number} answer
+		 */
+		log: function () {
+			// Todo: T109009
+		},
+		/**
+		 * Fired when one of the options are clicked.
+		 * @param {OO.ui.ButtonOptionWidget} btn
+		 */
+		onChoose: function ( btn ) {
+			this.log( btn.data.answer );
+			/**
+			 * @event dismiss fired when any of the buttons in the survey are selected.
+			 */
+			this.emit( 'dismiss' );
+			this.setItem( this.finalPanel );
+		}
+	} );
+
+	// This always makes me sad... https://phabricator.wikimedia.org/T108655
+	mw.extQuickSurveys.views.QuickSurvey = QuickSurvey;
+}( jQuery ) );
