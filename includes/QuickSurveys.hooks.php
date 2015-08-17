1789de88621011afba577f10a7372c19bebe4a89
Rob Moen <rmoen@wikimedia.org>
2015-08-17 14:14:41 -0700

    Register enabled surveys as RL modules via hook
    This creates a module per survey configured with survey messages
    for server side translation.  This can also be responsible for
    registering any additional dependencies based on survey configuration.
    
    Additionally:
    - Fixed the QuickSurveysConfig so the config can be read
    - Creates language keys for link, and privacy-policy-link
    bug: T107586
    Change-Id: I8ab993130bf3fe60de461ecdcff66b494281c310

--- includes/QuickSurveys.hooks.php
+++ includes/QuickSurveys.hooks.php
@@ -9,6 +9,7 @@
 namespace QuickSurveys;
 
 use ConfigFactory;
+use ResourceLoader;
 
 class Hooks {
 	/**
@@ -19,19 +20,7 @@ class Hooks {
 	 * @return boolean
 	 */
 	public static function onResourceLoaderGetConfigVars( &$vars ) {
-		$config = ConfigFactory::getDefaultInstance()->makeConfig( 'quicksurveys' );
-		// Get configured surveys
-		$configuredSurveys = $config->has( 'QuickSurveysConfig' )
-			? $config->get( 'QuickSurveysConfig' )
-			: array();
-		$enabledQuickSurveys = array();
-		// Make enabled surveys available to the browser
-		foreach ( $configuredSurveys as $survey ) {
-			if ( $survey['enabled'] === true ) {
-				$enabledQuickSurveys[] = $survey;
-			}
-		}
-		$vars['wgEnabledQuickSurveys'] = $enabledQuickSurveys;
+		$vars['wgEnabledQuickSurveys'] = self::getEnabledSurveys();
 		return true;
 	}
 
@@ -47,4 +36,79 @@ class Hooks {
 		$out->addModules( 'ext.quicksurveys.init' );
 		return true;
 	}
+
+	/**
+	 * ResourceLoaderRegisterModules hook handler
+	 *
+	 * Registers needed modules for enabled surveys
+	 *
+	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/ResourceLoaderRegisterModules
+	 *
+	 * @param ResourceLoader &$resourceLoader The ResourceLoader object
+	 * @return bool Always true
+	 */
+	public static function onResourceLoaderRegisterModules( ResourceLoader &$resourceLoader ) {
+		$enabledSurveys = self::getEnabledSurveys();
+		// Register enabled surveys as their own modules
+		foreach ( $enabledSurveys as $survey ) {
+			$messages = array();
+
+			// All surveys have a description
+			$messages[] = $survey['description'];
+
+			// Add messages that are specific the survey type
+			if ( $survey['type'] === 'internal' ) {
+				$messages[] = $survey['question'];
+				$messages[] = $survey['answers']['positive'];
+				$messages[] = $survey['answers']['neutral'];
+				$messages[] = $survey['answers']['negative'];
+
+			} elseif ( $survey['type'] === 'external' ) {
+				$messages[] = $survey['link'];
+				$messages[] = $survey['privacy-policy-link'];
+				$messages[] = $survey['privacy-policy-text'];
+			}
+
+			$surveyModule = array( $survey['module'] => array(
+				'messages' => $messages,
+				'targets' => array( 'desktop', 'mobile' ),
+			) );
+
+			$resourceLoader->register( $surveyModule );
+		}
+
+		return true;
+	}
+
+	/**
+	 * Helper method for getting enabled quick surveys
+	 *
+	 * @return array Enabled survey configuration array
+	 */
+	private static function getEnabledSurveys() {
+		$config = ConfigFactory::getDefaultInstance()->makeConfig( 'quicksurveys' );
+		// Get configured surveys
+		$configuredSurveys = $config->has( 'QuickSurveysConfig' )
+			? $config->get( 'QuickSurveysConfig' )
+			: array();
+		$enabledQuickSurveys = array();
+		// Make enabled surveys available to the browser
+		foreach ( $configuredSurveys as $survey ) {
+			if ( $survey['enabled'] === true ) {
+				$survey['module'] = self::getSurveyModuleName( $survey );
+				$enabledQuickSurveys[] = $survey;
+			}
+		}
+		return $enabledQuickSurveys;
+	}
+
+	/**
+	 * Returns the name of the specified survey's module
+	 *
+	 * @return string Survey's ResourceLoader module name
+	 */
+	private static function getSurveyModuleName( $survey ) {
+		return 'ext.quicksurveys.survey.' . str_replace( ' ', '.', $survey['name'] );
+	}
+
 }
