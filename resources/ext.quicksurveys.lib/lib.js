wikimedia/mediawiki-extensions-QuickSurveys

commit c27222bd96f6affaec41d07b4c72261203dad097

Update QuickSurveys to new structure of Minerva
This ignores the impact on cached HTML. If we need to run a survey
in January/early February we may need to rethink this because of the 90
days (either cache purge or add backwards compatiblity)
Changes:
 * Insert panel at the correct level
 * Update testing templates
 * Update qunit tests

Bug: T122516
Change-Id: I1e5c5af8066a5b905b54580e459b0651387c86d3

resources/ext.quicksurveys.lib/lib.js
@@ -20,7 +20,7 @@
 
 		if ( isMobileLayout ) {
 			// Find a paragraph in the first section to insert after
-			$place = $bodyContent.find( '> div' ).eq( 0 ).find( ' > p' ).eq( 0 );
+			$place = $bodyContent.find( '> div > div' ).eq( 0 ).find( ' > p' ).eq( 0 );
 		}
 
 		if ( $place && $place.length ) {
@@ -34,11 +34,11 @@
 			if ( $place.length ) {
 				$panel.insertBefore( $place );
 			} else {
-				// Insert in this after the first paragraph (for pages with just one paragraph
-				//   or the lead section/content container when no suitable element can be found (empty pages)
+				// Insert this after the first paragraph (for pages with just one paragraph)
+				// or the lead section/content container when no suitable element can be found (empty pages)
 				$place = $bodyContent.find( '> div > p' ).eq( 0 );
-				// Do this test separately for cases where no paragraphs
-				// elements are returned in document order so > div would always come first.
+				// Do this test separately for cases where no paragraph elements
+				// are returned in document order so > div would always come first.
 				// See http://blog.jquery.com/2009/02/20/jquery-1-3-2-released/
 				if ( !$place.length ) {
 					// Note that this will only ever happen if you have an article with no headings

tests/qunit/ext.quicksurveys.lib/test_lib.js
@@ -24,9 +24,9 @@
 
 		assert.ok( this.isPanelElement( $locationVector.find( '#mw-content-text' ).children().eq( 1 ) ),
 			'Check on desktop page it is inserted in correct place (before infobox)' );
-		assert.ok( this.isPanelElement( $locationMinerva.find( '> div' ).children().eq( 4 ) ),
+		assert.ok( this.isPanelElement( $locationMinerva.find( '> div > div' ).children().eq( 4 ) ),
 			'Check on mobile page it is inserted in correct place (after first paragraph)' );
-		assert.ok( this.isPanelElement( $locationMinervaTablet.find( '> div' ).children().eq( 1 ) ),
+		assert.ok( this.isPanelElement( $locationMinervaTablet.find( '> div > div' ).children().eq( 1 ) ),
 			'Check on mobile page it is inserted in correct place (before infobox)' );
 	} );
 
@@ -43,9 +43,9 @@
 
 		assert.ok( this.isPanelElement( $locationVector.find( '> div' ).eq( 0 ).children().eq( 1 ) ),
 			'Check it is inserted in correct place on vector (after first paragraph)' );
-		assert.ok( this.isPanelElement( $locationMinervaTablet.find( '> div' ).eq( 0 ).children().eq( 1 ) ),
+		assert.ok( this.isPanelElement( $locationMinervaTablet.find( '> div > div' ).eq( 0 ).children().eq( 1 ) ),
 			'Check it is inserted in correct place (before image)' );
-		assert.ok( this.isPanelElement( $locationMinerva.find( '> div' ).eq( 0 ).children().eq( 1 ) ),
+		assert.ok( this.isPanelElement( $locationMinerva.find( '> div > div' ).eq( 0 ).children().eq( 1 ) ),
 			'Check it is inserted in correct place (after first paragraph)' );
 	} );
 
@@ -65,11 +65,11 @@
 			'Check only one panel got added.' );
 		assert.ok( $locationMinerva.find( '.test-panel' ).length === 1,
 			'Check only one panel got added.' );
-		assert.ok( this.isPanelElement( $locationMinerva.find( '> div' ).eq( 0 ).children().eq( 1 ) ),
+		assert.ok( this.isPanelElement( $locationMinerva.find( '> div > div' ).eq( 0 ).children().eq( 1 ) ),
 			'Check it is inserted in correct place on mobile (after first paragraph)' );
 		assert.ok( this.isPanelElement( $locationVector.find( '> div' ).eq( 0 ).children().eq( 1 ) ),
 			'Check it is inserted in correct place on vector (after first paragraph)' );
-		assert.ok( this.isPanelElement( $locationMinervaTablet.find( '> div' ).eq( 0 ).children().eq( 1 ) ),
+		assert.ok( this.isPanelElement( $locationMinervaTablet.find( '> div > div' ).eq( 0 ).children().eq( 1 ) ),
 			'Check it is inserted in correct place on tablet (after first paragraph)' );
 	} );
 
@@ -83,11 +83,11 @@
 		qSurveys._insertPanel( $locationMinerva, this.getPanel(), true );
 		qSurveys._insertPanel( $locationMinervaTablet, this.getPanel() );
 
-		assert.ok( this.isPanelElement( $locationMinerva.find( '> div' ).eq( 1 ) ),
+		assert.ok( this.isPanelElement( $locationMinerva.find( '> div > div' ).eq( 1 ) ),
 			'Check it is inserted in correct place on mobile (before first heading)' );
 		assert.ok( this.isPanelElement( $locationVector.find( '> div' ).children().eq( 1 ) ),
 			'Check it is inserted in correct place on vector (before first heading)' );
-		assert.ok( this.isPanelElement( $locationMinervaTablet.find( '> div' ).eq ( 1 ) ),
+		assert.ok( this.isPanelElement( $locationMinervaTablet.find( '> div > div' ).eq ( 1 ) ),
 			'Check it is inserted in correct place on tablet (before first heading)' );
 	} );
 

tests/qunit/ext.quicksurveys.lib/templates/minerva-1.html
@@ -1,13 +1,15 @@
-<div id="bodyContent">
-	<div>
-		<div class="hatnote">hatnote text</div>
-		<table class="infobox"></table>
-		<table class="infobox"></table>
-		<p>first paragraph</p>
-		<p>second paragraph</p>
-	</div>
-	<h2>first heading</h2>
-	<div>
-		<p>first paragraph section 1</p>
+<div id="bodyContent" class="content">
+	<div id="mw-content-text" lang="en" dir="ltr" class="mw-content-ltr">
+		<div>
+			<div class="hatnote">hatnote text</div>
+			<table class="infobox"></table>
+			<table class="infobox"></table>
+			<p>first paragraph</p>
+			<p>second paragraph</p>
+		</div>
+		<h2>first heading</h2>
+		<div>
+			<p>first paragraph section 1</p>
+		</div>
 	</div>
 </div>

tests/qunit/ext.quicksurveys.lib/templates/minerva-2.html
@@ -1,12 +1,14 @@
-<div id="bodyContent">
-	<div>
-		<p>first paragraph</p>
-		<div class="thumb tright"></div>
-		<p>second paragraph</p>
-		<p>third paragraph</p>
-	</div>
-	<h2>first heading</h2>
-	<div>
-		<p>first paragraph section 1</p>
+<div id="bodyContent" class="content">
+	<div id="mw-content-text" lang="en" dir="ltr" class="mw-content-ltr">
+		<div>
+			<p>first paragraph</p>
+			<div class="thumb tright"></div>
+			<p>second paragraph</p>
+			<p>third paragraph</p>
+		</div>
+		<h2>first heading</h2>
+		<div>
+			<p>first paragraph section 1</p>
+		</div>
 	</div>
 </div>

tests/qunit/ext.quicksurveys.lib/templates/minerva-3.html
@@ -1,7 +1,9 @@
-<div id="bodyContent">
-	<div>
-		<p>first paragraph</p>
-		<p>second paragraph</p>
-		<p>third paragraph</p>
+<div id="bodyContent" class="content">
+	<div id="mw-content-text" lang="en" dir="ltr" class="mw-content-ltr">
+		<div>
+			<p>first paragraph</p>
+			<p>second paragraph</p>
+			<p>third paragraph</p>
+		</div>
 	</div>
 </div>

tests/qunit/ext.quicksurveys.lib/templates/minerva-4.html
@@ -1,15 +1,18 @@
-<div id="bodyContent">
-	<div></div>
-	<h2>Heading</h2>
-	<div>
-		<p>first paragraph</p>
-		<p>second paragraph</p>
-		<p>third paragraph</p>
-	</div>
-	<h2>Heading</h2>
-	<div>
-		<p>first paragraph</p>
-		<p>second paragraph</p>
-		<p>third paragraph</p>
-	</div>
+<div id="bodyContent" class="content">
+	<div id="mw-content-text" lang="en" dir="ltr" class="mw-content-ltr">
+		<div id="bodyContent">
+			<div></div>
+			<h2>Heading</h2>
+			<div>
+				<p>first paragraph</p>
+				<p>second paragraph</p>
+				<p>third paragraph</p>
+			</div>
+			<h2>Heading</h2>
+			<div>
+				<p>first paragraph</p>
+				<p>second paragraph</p>
+				<p>third paragraph</p>
+			</div>
+		</div>
 </div>

commit 6eeffdb021ffcb459be26ee7b0fd21cac9450b41

Fix the failing QUnit tests
c27222b introduced test failures in the tablet-related Placement (image)
and Placement (no headings) cases.

Changes:
* Follow up on 213d845 and make the test results more readable too
* Break apart the long catch-most selector in #insertPanel and comment
  the Minerva-specific part
* Add a new selector to the list, which fixes the Placement (image) case
  by actually reflecting Minerva's DOM structure
* Fix the Placement (no headings) case by relaxing the requirement that
  the first paragraph of an article should be an immediate child of a
  div.

Note well that in some the browser tests, we are currently testing
placement on a stub article viewed on a tablet device, the
Placement (no headings) case and corresponding fix cover this.

Bug: T123593
Change-Id: I5b4e19e8669426879ed631ba0200bb0873f76e78

resources/ext.quicksurveys.lib/lib.js
@@ -27,8 +27,19 @@
 			$panel.insertAfter( $place );
 		} else {
 			$place = $bodyContent
-				// Account for the Mobile Frontend section wrapper around .thumb.
-				.find( '.infobox, > div > .thumb, > .thumb, h1, h2, h3, h4, h5, h6' )
+				.find(
+					[
+						'.infobox',
+
+						// Account for the Mobile Frontend section wrapper
+						// around .thumb.
+						'> div > div > .thumb',
+
+						'> div > .thumb',
+						'> .thumb',
+						'h1, h2, h3, h4, h5, h6'
+					].join( ',' )
+				)
 				.filter( ':not(.toc h2)' )
 				.eq( 0 );
 			if ( $place.length ) {
@@ -36,7 +47,7 @@
 			} else {
 				// Insert this after the first paragraph (for pages with just one paragraph)
 				// or the lead section/content container when no suitable element can be found (empty pages)
-				$place = $bodyContent.find( '> div > p' ).eq( 0 );
+				$place = $bodyContent.find( 'p' ).eq( 0 );
 				// Do this test separately for cases where no paragraph elements
 				// are returned in document order so > div would always come first.
 				// See http://blog.jquery.com/2009/02/20/jquery-1-3-2-released/

tests/qunit/ext.quicksurveys.lib/test_lib.js
@@ -59,14 +59,14 @@
 		qSurveys._insertPanel( $locationMinerva, this.getPanel(), true );
 		qSurveys._insertPanel( $locationMinervaTablet, this.getPanel() );
 
-		assert.ok( $locationVector.find( '.test-panel' ).length === 1,
-			'Check only one panel got added.' );
-		assert.ok( $locationMinerva.find( '.test-panel' ).length === 1,
-			'Check only one panel got added.' );
-		assert.ok( $locationMinervaTablet.find( '.test-panel' ).length === 1,
-			'Check only one panel got added.' );
+		assert.equal( $locationVector.find( '.test-panel' ).length, 1,
+			'Check only one panel got added on Vector' );
+		assert.equal( $locationMinerva.find( '.test-panel' ).length, 1,
+			'Check only one panel got added on mobile' );
+		assert.equal( $locationMinervaTablet.find( '.test-panel' ).length, 1,
+			'Check only one panel got added on tablet' );
 		assert.ok( this.isPanelElement( $locationVector.find( '#firstp' ).next() ),
-			'Check it is inserted in correct place on vector (after first paragraph)' );
+			'Check it is inserted in correct place on Vector (after first paragraph)' );
 		assert.ok( this.isPanelElement( $locationMinerva.find( '#firstp' ).next() ),
 			'Check it is inserted in correct place on mobile (after first paragraph)' );
 		assert.ok( this.isPanelElement( $locationMinervaTablet.find( '#firstp' ).next() ),