50ad3eda0ebb001449bd8d295d4ef504c7cc70e5
Umherirrender <umherirrender_de.wp@web.de>
2023-04-03 23:06:45 +0200

    build: Updating npm dependencies
    * eslint-config-wikimedia: 0.22.1 → 0.24.0
    * @wikimedia/mw-node-qunit: 6.2.1 → 7.0.0
    * @wikimedia/codex: 0.1.0 → 0.8.0
    * vue: 3.2.33 → 3.2.37
    
    Change-Id: I25e51919d6c0d64fef4970d7e0d0369477938bd0

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,10 @@
 'use strict';
-
+/* eslint-disable node/no-unsupported-features/node-builtins */
 const wikimediaTestingUtils = require( '@wikimedia/mw-node-qunit' );
+
+/* https://github.com/jsdom/jsdom/issues/2524 */
+const { TextEncoder, TextDecoder } = require( 'util' );
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
 
 wikimediaTestingUtils.setUp( false );
