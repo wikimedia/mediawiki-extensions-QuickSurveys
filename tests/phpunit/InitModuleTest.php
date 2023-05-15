96bb808de485724f6e5fa68c1f43175165b133af
Jon Robson <jdlrobson@gmail.com>
2023-05-15 08:21:10 -0700

    Remove test that's not a unit test
    Not sure what this is trying to test, but it's not a unit
    test if it's relying on upstream code in core.
    
    Change-Id: Ic4952be9cac3da98d4d83dc305e7205138ef7ff8

--- tests/phpunit/InitModuleTest.php
+++ tests/phpunit/InitModuleTest.php
@@ -19,20 +19,6 @@ class InitModuleTest extends \MediaWikiIntegrationTestCase {
 		$context = $this->createMock( Context::class );
 		$module = new InitModule( [], __DIR__ . '/../..' );
 
-		$this->assertSame( [
-			'main' => 'resources/ext.quicksurveys.init/init.js',
-			'files' => [
-				'resources/ext.quicksurveys.init/init.js' => [
-					'type' => 'script',
-					'content' => <<<'SCRIPT'
-const forcedSurvey = mw.util.getParamValue( 'quicksurvey' );
-
-require( 'ext.quicksurveys.lib' ).showSurvey( forcedSurvey );
-
-SCRIPT
-				]
-			],
-		], $module->getPackageFiles( $context ) );
 		$this->assertSame( [ 'ext.quicksurveys.lib' ], $module->getDependencies() );
 		$this->assertSame( [ 'desktop', 'mobile' ], $module->getTargets() );
 	}
