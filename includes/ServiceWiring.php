834ebf7242ad3cebb965c0a6077865caab711156
Jon Robson <jdlrobson@gmail.com>
2026-01-27 13:19:48 -0800

    Add QuickSurveysSurveys hook
    This will allow extensions to easily add surveys and manage
    their own configuration. This makes it easier to obtain
    translations.
    
    Bug: T415721
    Change-Id: I311d8c558cc59d901ff111828fc69474ef28f246

--- includes/ServiceWiring.php
+++ includes/ServiceWiring.php
@@ -19,13 +19,22 @@ return [
 		 * @return \QuickSurveys\Survey[] List of active surveys to be selected from on the client
 		 */
 		static function ( MediaWikiServices $services ): array {
+			$hookContainer = $services->getHookContainer();
 			$config = $services->getService( 'QuickSurveys.Config' );
 			$configuredSurveys = $config->has( 'QuickSurveysConfig' )
 				? $config->get( 'QuickSurveysConfig' )
 				: [];
 			$logger = LoggerFactory::getInstance( 'QuickSurveys' );
-
+			// The hook allows addition of surveys. You cannot remove surveys defined by configuration.
+			$hookSurveys = [];
+			$hookContainer->run( 'QuickSurveysEnabled', [ &$hookSurveys ], [] );
+			// Iterate through the all surveys and make sure they all have unique
+			// names. Where they do the configured survey overrides the hook survey.
+			$uniqueSurveys = [];
+			foreach ( array_merge( $hookSurveys, $configuredSurveys ) as $survey ) {
+				$uniqueSurveys[ $survey['name'] ] = $survey;
+			}
 			$factory = new SurveyFactory( $logger );
-			return $factory->parseSurveyConfig( $configuredSurveys );
+			return $factory->parseSurveyConfig( array_values( $uniqueSurveys ) );
 		}
 ];
